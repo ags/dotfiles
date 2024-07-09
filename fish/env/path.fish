92fbe4af9f993998edfd5528bf05e2081b008743
Alex Smith <ags@fastmail.fm>
2024-07-09 11:12:24 +1000

    pg in path

--- fish/env/path.fish
+++ fish/env/path.fish
@@ -1,3 +1,8 @@
 set -x GOPATH $HOME/Code/go
 
 set -x PATH $PATH $GOPATH/bin
+
+fish_add_path /opt/homebrew/opt/libpq/bin
+
+set -gx LDFLAGS "-L/opt/homebrew/opt/libpq/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/libpq/include"
