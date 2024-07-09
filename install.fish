98bf397f39271c5542b6393f200b0e5bebbfb98a
Alex Smith <ags@fastmail.fm>
2024-07-09 11:10:59 +1000

    Add missing links

--- install.fish
+++ install.fish
@@ -5,3 +5,18 @@ function link
 end
 
 link $PWD/asdfrc $HOME/.asdfrc
+
+link $PWD/gitattributes $HOME/.gitattributes
+link $PWD/gitignore $HOME/.gitignore
+link $PWD/gitconfig $HOME/.gitconfig
+link $PWD/githelpers $HOME/.githelpers
+
+link $PWD/psqlrc $HOME/.psqlrc
+
+link $PWD/ptconfig.toml $HOME/.ptconfig.toml
+
+link $PWD/tmux.conf $HOME/.tmux.conf
+
+link $PWD/fish $HOME/.config
+
+link $PWD/nvim $HOME/.config
