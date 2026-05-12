b747c981820326dd5536ed996dd0603827be1d6e
Alex Smith <ags@fastmail.fm>
2026-05-12 13:40:16 +1000

    alias for bk current branch
    thanks claire

--- fish/config.fish
+++ fish/config.fish
@@ -21,5 +21,7 @@ alias gpr "gh pr create -d && gh pr view -w"
 
 alias retag "/opt/homebrew/bin/ctags -R --exclude=.git --exclude=log --exclude=tmp --exclude=node_modules"
 
+alias bk "open https://www.buildkite.com/conversation/(basename (git rev-parse --show-toplevel))/builds?branch=(git branch --show-current)"
+
 if [ -f '/Users/ags/Code/google-cloud-sdk/path.fish.inc' ]; . '/Users/ags/Code/google-cloud-sdk/path.fish.inc'; end
 fish_add_path $HOME/.local/bin
